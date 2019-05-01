836d28c4932dc3d1ae6839a4ad5547d12e5661dc
Rishitosh210 <40652479+Rishitosh210@users.noreply.github.com>
2019-05-01 08:34:41 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,10 +1,10 @@
-# from flask import Flask
+from flask import Flask
 
-# app = Flask(__name__)
+app = Flask(__name__)
 
-# @app.route("/")
-# def home():
-#     return "Hello, World!"
+@app.route("/")
+def home():
+    return "Hello, World!"
 
-# if __name__ == "__main__":
-#     app.run(debug=True)
+if __name__ == "__main__":
+    app.run(debug=True)
