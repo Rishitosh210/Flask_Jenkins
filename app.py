281bccc4729a4ef88bdcf83fda36e53709722518
AmanRawal <aman.rawal32@gmail.com>
2019-05-01 09:29:51 +0530

    changes to app

--- app.py
+++ app.py
@@ -1,20 +1,18 @@
-# from flask import Flask
+from flask import Flask
 
-# app = Flask(__name__)
+app = Flask(__name__)
 
-<<<<<<< HEAD
+# <<<<<<< HEAD
 @app.route("/")
 def home():
-<<<<<<< HEAD
+# <<<<<<< HEAD
     return "Hello, Rishi!"
-=======
-# @app.route("/")
-# def home():
-#     return "Hello, World!"
->>>>>>> 1037957823145a544496537ebfbc9539c57f7fb5
-=======
+# =======
+@app.route("/")
+def home():
     return "Hello, World!"
->>>>>>> parent of d0a1eee... Jenkins check
 
-# if __name__ == "__main__":
-#     app.run(debug=True)
+    # return "Hello, World!"
+
+if __name__ == "__main__":
+    app.run(debug=True)
