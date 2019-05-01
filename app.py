586770c8e4aa92b328769d05b7c7371e7efa8c5b
AmanRawal <aman.rawal32@gmail.com>
2019-05-01 09:25:49 +0530

    Revert "Jenkins check"
    This reverts commit d0a1eeef00a7083136bc3326e7dc501d7d72011e.

--- app.py
+++ app.py
@@ -5,12 +5,16 @@
 <<<<<<< HEAD
 @app.route("/")
 def home():
+<<<<<<< HEAD
     return "Hello, Rishi!"
 =======
 # @app.route("/")
 # def home():
 #     return "Hello, World!"
 >>>>>>> 1037957823145a544496537ebfbc9539c57f7fb5
+=======
+    return "Hello, World!"
+>>>>>>> parent of d0a1eee... Jenkins check
 
 # if __name__ == "__main__":
 #     app.run(debug=True)
