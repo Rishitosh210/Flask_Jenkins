ba7c707e911b9e755b8084fcb6b9d6151e4e51dd
Rishitosh210 <40652479+Rishitosh210@users.noreply.github.com>
2020-06-20 17:14:24 -0400

    Update app.py

--- app.py
+++ app.py
@@ -2,17 +2,11 @@ from flask import Flask
 
 app = Flask(__name__)
 
-# <<<<<<< HEAD
+
 @app.route("/")
 def home():
 # <<<<<<< HEAD
-    return "Hello, Rishi!"
-# =======
-# @app.route("/")
-# def home():
-#     return "Hello, World!"
-
-    # return "Hello, World!"
+    return "Hello, Seth!"
 
 if __name__ == "__main__":
     app.run(debug=True)
